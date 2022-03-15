49e2709b0dae81e3a4a3e399296f6c10e481bda7
João Oliveira <joao.p.oliveira@ua.pt>
2022-03-15 09:59:24 +0000

    Função vInit feita

--- Primeiro.c
+++ Primeiro.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+
+static int vect[100];
+
+void vInit(int *vect)
+{
+	for(int i=0;i<(sizeof(vect)/sizeof(vect[0]));i++)
+		vect[i] = i+1;
+	
+}
+
+
+
 int main(void)
 {
+	vInit(vect);
+
 	return 0;
 }
